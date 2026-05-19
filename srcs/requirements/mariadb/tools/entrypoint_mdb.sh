90ee736d4f1d6e048371d431783c98dc4179c655
Piccolo-42 <stef.rvz@gmail.com>
2026-05-19 17:17:14 +0200

    entrypoint

--- srcs/requirements/mariadb/tools/entrypoint_mdb.sh
+++ srcs/requirements/mariadb/tools/entrypoint_mdb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #check if database is already init
-if [ ! -d "/var/lib/mysql/.init" ]; then
+if [ ! -f "/var/lib/mysql/.init" ]; then
 	DB_PASSWORD=$(cat /run/secrets/db_password)
 	DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 	mysql_install_db --datadir=/var/lib/mysql --user=mysql
@@ -13,7 +13,7 @@ CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-touch .init
+touch /var/lib/mysql/.init
 fi
 
 #launch mariadb 
