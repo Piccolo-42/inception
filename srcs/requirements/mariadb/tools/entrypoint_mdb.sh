2184daa98bbde5dadd680781a2cd9beceb14e856
Piccolo-42 <stef.rvz@gmail.com>
2026-05-23 11:56:59 +0200

    healthcheck

--- srcs/requirements/mariadb/tools/entrypoint_mdb.sh
+++ srcs/requirements/mariadb/tools/entrypoint_mdb.sh
@@ -9,6 +9,7 @@ if [ ! -f "/var/lib/mysql/.init" ]; then
 	DB_PASSWORD=$(cat /run/secrets/db_password)
 	DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 	mysql_install_db --datadir=/var/lib/mysql --user=mysql
+	
 	mysqld --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
